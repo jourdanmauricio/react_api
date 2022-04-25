f821fad33993e6a6fdb34890aeea83736db95942
Mauricio <jourdanmauricio@gmail.com>
2022-04-25 03:15:09 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CrudApi from './components/CrudApi';
+import CrudApp from "./components/CrudApp";
+
+function App() {
+  return (
+    <>
+      <h1>Ejercicios con React</h1>
+      <CrudApi />
+      <hr />
+      <CrudApp />
+    </>
+  );
+}
+
+export default App;
