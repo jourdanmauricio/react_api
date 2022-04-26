8e58bedbee985834bfff6f9f577b20912e234abe
Mauricio <jourdanmauricio@gmail.com>
2022-04-26 09:10:23 -0300

    Add validation form

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import CrudApi from "./components/CrudApi";
 import CrudApp from "./components/CrudApp";
 import SelectsAnidados from "./components/SelectsAnidados";
 import SongSearch from "./components/SongSearch";
+import ContactForm from "./components/ContactForm";
 
 function App() {
   return (
     <>
       <h1>Ejercicios con React</h1>
+      <ContactForm />
+      <hr />
       <SelectsAnidados />
       <hr />
       <SongSearch />
